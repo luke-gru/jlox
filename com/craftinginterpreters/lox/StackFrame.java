82f5b2c427cbd2034a85ad3af23d0353edaa922b
Luke Gruber <luke.gru@gmail.com>
2018-05-29 13:17:41 -0400

    fix StackFrame#toString() bug

--- com/craftinginterpreters/lox/StackFrame.java
+++ com/craftinginterpreters/lox/StackFrame.java
@@ -13,12 +13,24 @@ class StackFrame {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         if (stmtOrCallable instanceof Stmt.Throw) {
-            builder.append("<throw>");
+            builder.append("<!throw>");
+        } else if (stmtOrCallable instanceof Stmt.Function) {
+            Stmt.Function func = (Stmt.Function)stmtOrCallable;
+            // TODO: format the function name different if it's a method, the function type, etc.
+            String funcName = null;
+            if (func.name != null) {
+                funcName = func.name.lexeme;
+            } else {
+                funcName = "(anon)";
+            }
+            builder.append("<" + funcName + ">");
         } else if (stmtOrCallable instanceof LoxCallable) {
             LoxCallable func = (LoxCallable)stmtOrCallable;
             builder.append(func.toString());
         } else {
-            throw new RuntimeException("bad class given to stackframe. Must be Throw or Callable. BUG");
+            throw new RuntimeException("bad class given to stackframe. " +
+                "Must be Throw or Callable, is: " +
+                stmtOrCallable.getClass().getName() +  ". BUG");
         }
         if (token != null) {
             builder.append(" at line " + token.line + ".");
